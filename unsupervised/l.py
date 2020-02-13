1092b2d9f499b7755216baaaa27d1880d506d70d
Nfor Esperance <nforesperance1@gmail.com>
2020-02-13 12:28:14 +0100

    kmeans from scrath dynamic

--- unsupervised/l.py
+++ unsupervised/l.py
@@ -1,45 +1,140 @@
 import matplotlib.pyplot as plt
 from matplotlib import style
+from sklearn.datasets import make_blobs
 style.use('ggplot')
 import numpy as np
 
-X = np.array([[1, 2],
-              [1.5, 1.8],
-              [5, 8 ],
-              [8, 8],
-              [1, 0.6],
-              [9,11]])
+# X = np.array([[1, 2],
+#               [1.5, 1.8],
+#               [5, 8 ],
+#               [8, 8],
+#               [1, 0.6],
+#               [9,11],
+#               [8,2],
+#               [10,2],
+#               [9,3],])
 
-# plt.scatter(X[:,0], X[:,1], s=150)
-# plt.show()
+##plt.scatter(X[:,0], X[:,1], s=150)
+##plt.show()
+X, y = make_blobs(n_samples=15, centers=3, n_features=2)
+
 colors = 10*["g","r","c","b","k"]
 
-class K_Means:
-    def __init__(self, k=2, tol=0.001, max_iter=300):
-        self.k = k
-        self.tol = tol
-        self.max_iter = max_iter
+class Mean_Shift:
+    def __init__(self, radius=None, radius_norm_step = 100):
+        self.radius = radius
+        self.radius_norm_step = radius_norm_step
 
-    def fit(self,data):
+    def fit(self, data):
 
-        self.centroids = {}
+        if self.radius == None:
+            all_data_centroid = np.average(data, axis=0)
+            all_data_norm = np.linalg.norm(all_data_centroid)
+            self.radius = all_data_norm / self.radius_norm_step
+        centroids = {}
 
-        for i in range(self.k):
-            self.centroids[i] = data[i]
+        for i in range(len(data)):
+            centroids[i] = data[i]
 
-        for i in range(self.max_iter):
-            self.classifications = {}
+    # [::-1] reverses the  list
+        weights = [i for i in range(self.radius_norm_step)][::-1]    
+        while True:
+            new_centroids = []
+            for i in centroids:
+                in_bandwidth = []
+                centroid = centroids[i]
+                
+                for featureset in data:
+                    #if np.linalg.norm(featureset-centroid) < self.radius:
+                    #    in_bandwidth.append(featureset)
+                    distance = np.linalg.norm(featureset-centroid)
+                    if distance == 0:
+                        distance = 0.00000000001
+                    weight_index = int(distance/self.radius)
+                    if weight_index > self.radius_norm_step-1:
+                        weight_index = self.radius_norm_step-1
 
-            for i in range(self.k):
-                self.classifications[i] = []
+                    to_add = (weights[weight_index]**2)*[featureset]
+                    in_bandwidth +=to_add
+                    
 
-            for featureset in data:
-                distances = [np.linalg.norm(featureset-self.centroids[centroid]) for centroid in self.centroids]
-                classification = distances.index(min(distances))
-                self.classifications[classification].append(featureset)
+                new_centroid = np.average(in_bandwidth,axis=0)
+                new_centroids.append(tuple(new_centroid))
 
-            prev_centroids = dict(self.centroids)
+            uniques = sorted(list(set(new_centroids)))
 
-            for classification in self.classifications:
-                self.centroids[classification] = np.average(self.classifications[classification],axis=0)
+            to_pop = []
+
+            for i in uniques:
+                for ii in [i for i in uniques]:
+                    if i == ii:
+                        pass
+                    elif np.linalg.norm(np.array(i)-np.array(ii)) <= self.radius:
+                        #print(np.array(i), np.array(ii))
+                        to_pop.append(ii)
+                        break
+
+            for i in to_pop:
+                try:
+                    uniques.remove(i)
+                except:
+                    pass
+
+            prev_centroids = dict(centroids)
+
+            centroids = {}
+            for i in range(len(uniques)):
+                centroids[i] = np.array(uniques[i])
+
+            optimized = True
+
+            for i in centroids:
+                if not np.array_equal(centroids[i], prev_centroids[i]):
+                    optimized = False
+                if not optimized:
+                    break
+                
+            if optimized:
+                break
+
+        self.centroids = centroids
+
+        self.classifications = {}
+
+        for i in range(len(self.centroids)):
+            self.classifications[i] = []
             
+        for featureset in data:
+            #compare distance to either centroid
+            distances = [np.linalg.norm(featureset-self.centroids[centroid]) for centroid in self.centroids]
+            #print(distances)
+            classification = (distances.index(min(distances)))
+
+            # featureset that belongs to that cluster
+            self.classifications[classification].append(featureset)
+
+    def predict(self,data):
+        #compare distance to either centroid
+        distances = [np.linalg.norm(data-self.centroids[centroid]) for centroid in self.centroids]
+        classification = (distances.index(min(distances)))
+        return classification
+
+
+
+clf = Mean_Shift()
+clf.fit(X)
+
+centroids = clf.centroids
+print(centroids)
+
+colors = 10*['r','g','b','c','k','y']
+
+for classification in clf.classifications:
+    color = colors[classification]
+    for featureset in clf.classifications[classification]:
+        plt.scatter(featureset[0],featureset[1], marker = "x", color=color, s=150, linewidths = 5, zorder = 10)
+
+for c in centroids:
+    plt.scatter(centroids[c][0],centroids[c][1], color='k', marker = "*", s=150, linewidths = 5)
+
+plt.show()
